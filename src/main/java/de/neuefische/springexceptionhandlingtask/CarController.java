dfbc9cc6095e455f57dbe8f0126eab60cf5490d6
ursulakohler <kohlerursula22@gmail.com>
2024-07-24 12:14:19 +0200

    Car Controller exception

--- src/main/java/de/neuefische/springexceptionhandlingtask/CarController.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/CarController.java
@@ -1,5 +1,6 @@
 package de.neuefische.springexceptionhandlingtask;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
@@ -19,5 +20,18 @@ public class CarController {
     @GetMapping
     String getAllCars() {
         throw new NoSuchElementException("No Cars found");
+    }
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleGlobalException2(Exception ex) {
+        ErrorMessage errorMessage = new ErrorMessage("Ein Fehler ist aufgetreten, kein Auto gefunden: " + ex.getMessage());
+        return errorMessage;
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleGlobalException(Exception ex) {
+        ErrorMessage errorMessage = new ErrorMessage("Ein Fehler ist aufgetreten: " + ex.getMessage());
+        return errorMessage;
     }
 }
